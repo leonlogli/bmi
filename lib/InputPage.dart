9616da896770c7793e6ee9a5f4e0d4276193f1bc
Léon Logli <leonlogli@gmail.com>
2020-11-04 01:25:52 +0100

    Refacor Widgets: add BmiCard

---
+++ lib/InputPage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: BmiCard(
+                  bgColor: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: BmiCard(
+                  bgColor: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: BmiCard(
+              bgColor: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: BmiCard(
+                  bgColor: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: BmiCard(
+                  bgColor: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class BmiCard extends StatelessWidget {
+  final Color bgColor;
+
+  BmiCard({
+    @required this.bgColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: bgColor,
+      ),
+    );
+  }
+}
